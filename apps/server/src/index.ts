d685522d572c6118755c59da3160be97b2e197f3
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-03-05 20:49:35 +0700

    test r2 upload

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,9 +4,22 @@ import { drizzle } from 'drizzle-orm/d1';
 const app = new Hono<{Bindings:Env}>()
 
 
-app.get('/', (c) => {
+app.get('/', async(c) => {
+  return c.json({ message: 'Hello World' })
+})
+
+app.post('/', async(c) => {
+  const formData = c.req.formData()
   const db = drizzle(c.env.DB)
-  console.log(db);
+  const storage = c.env.BUCKET
+  const file = (await formData).get("file") as File
+  try{
+    const res = await storage.put(file.name,file);
+    return c.json({ message: res })
+  }
+  catch(e){
+    console.log(e);
+  }
   
   return c.json({ message: c.env.DB })
 })
