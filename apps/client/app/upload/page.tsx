0cee4ff640342ecf7137b08d62b26327a0381947
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-03-15 18:13:00 +0700

    try to fix error

--- apps/client/app/upload/page.tsx
+++ apps/client/app/upload/page.tsx
@@ -4,7 +4,6 @@ import { api } from "@/utils/api";
 import { AxiosError } from "axios";
 import Link from "next/link";
 import { useRef, useState } from "react";
-import { File } from "node:buffer"
 
 export default function Page() {
 
